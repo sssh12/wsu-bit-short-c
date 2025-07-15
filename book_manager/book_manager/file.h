85addfe31fa77bc9f62eefd3e7627084fd060bfe
SeunghaOh <90227845+sssh12@users.noreply.github.com>
2025-07-15 09:22:06 +0900

    Delete book_manager directory

--- book_manager/book_manager/file.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void file_load();	// 파일 -> 배열로 읽어옴 (주소는 전역 books 사용)
-void file_save();	// 배열 -> 파일로 저장
--- book_manager/book_manager/print.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void print_logo();
-char print_menu();	// 선택값 반환
-void print_ending();
